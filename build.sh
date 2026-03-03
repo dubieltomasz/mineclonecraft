e27326d7fa1ad365ca287dd26bd48026aeb5f935
thomas <big_t@aSUS.localdomain>
2026-03-03 14:25:49 +0100

    Added CMakeLists.txt

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-clang++ -std=c++26 -target x86_64-w64-windows-gnu main.cpp -o build.exe $(pkg-config --cflags --libs sdl3)
--- buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-g++ -std=c++26 main.cpp -o tmp $(pkg-config --cflags --libs sdl3) && ./tmp && rm tmp
